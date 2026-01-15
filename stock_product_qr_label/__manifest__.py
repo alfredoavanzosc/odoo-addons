f5f810cf77c74195aa6a56db975116d8d4197c16
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-15 09:30:52 +0100

    [18.0][MIG] stock_product_qr_label: Migration to 18.0

--- stock_product_qr_label/__manifest__.py
+++ stock_product_qr_label/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Product QR Label",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/odoo-addons",
     "category": "Inventory",
