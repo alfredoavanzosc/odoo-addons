bd4e61de2e0366fdafb4542d5add7802294ba64b
Alfredo <alfredodelafuente@avanzosc.es>
2026-03-12 12:56:13 +0100

    [18.0][MIG] purchase_partner_company: Migration to 18.0

--- purchase_partner_company/__manifest__.py
+++ purchase_partner_company/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Partner Company",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Inventory/Purchase",
     "depends": [
         "purchase",
@@ -11,8 +11,6 @@
     "author": "AvanzOSC",
     "license": "AGPL-3",
     "website": "https://github.com/avanzosc/odoo-addons",
-    "data": [
-        "views/purchase_partner_company_view.xml",
-    ],
+    "data": [],
     "installable": True,
 }
