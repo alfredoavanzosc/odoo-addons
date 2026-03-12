9ce64c410357efce2c52e54dc4825b78af9f702f
Alfredo <alfredodelafuente@avanzosc.es>
2026-03-12 07:51:37 +0100

    [18.0][MIG] account_invoice_report_stamp: Migration to 18.0

--- account_invoice_report_stamp/__manifest__.py
+++ account_invoice_report_stamp/__manifest__.py
@@ -1,12 +1,14 @@
+# Copyright 2025 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "account_invoice_report_stamp",
-    "version": "16.0.1.0.0",
+    "name": "Account Invoice Report Stamp",
+    "version": "18.0.1.0.0",
     "category": "Custom",
-    "author": "Avanzosc",
     "license": "LGPL-3",
-    "depends": ["res_company_signature_fields"],
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["account", "res_company_signature_fields"],
     "data": ["views/account_report_views.xml"],
     "installable": True,
     "application": False,
-    "website": "https://github.com/avanzosc/odoo-addons",
 }
