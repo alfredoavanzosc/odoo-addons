925a6afcd0785ffa82f3e85e814e1a826c862713
Alfredo <alfredodelafuente@avanzosc.es>
2026-03-12 07:46:43 +0100

    [18.0][MIG] res_company_signature_fields: Migration to 18.0

--- res_company_signature_fields/__manifest__.py
+++ res_company_signature_fields/__manifest__.py
@@ -1,14 +1,16 @@
+# Copyright 2025 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Company Signature Fields",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Custom",
-    "author": "Avanzosc",
     "license": "LGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "depends": ["base"],
     "data": [
         "views/res_company_signature_fields_view.xml",
     ],
     "installable": True,
     "application": False,
-    "website": "https://github.com/avanzosc/odoo-addons",
 }
